d9be140f0539401329f496dd10d89efd9ff679ef
Carollina Moraes <carolmoraes333@gmail.com>
2023-06-17 17:33:49 -0300

    number, string, boolean

---
+++ type-boolean.js
@@ -0,0 +1,18 @@
+const firstNumber = 5;
+const secondNumber = 5;
+
+let cadastroAtiva = true;
+
+console.log(firstNumber === secondNumber);
+
+const texto1 = "Alura";
+const texto2 = "alura";
+
+console.log(texto1 === texto2);
+
+const cidade = "Rio de Janeiro";
+const input = "rio de janeiro";
+
+const inputMinusculo = input.toLowerCase();
+
+console.log(cidade === inputMinusculo); // true
